c93544c44de1f05a0dd5e406d9ad9c45352ceb50
23010101185 <148247055+23010101185@users.noreply.github.com>
2023-10-19 12:35:41 +0530

    Update ExALL.c

--- 13.B/ExALL.c
+++ 13.B/ExALL.c
@@ -5,21 +5,21 @@ void main(){
 	int i,j,k=1,a=0;
 	char A=65;
 	
-	for(i=1;i<=5;i++){				//a)Out put
-		for(j=1;j<=i;j++){			// 1
-			printf("%d ",k);		// 2 3
-			k++; 	 				// 4 5 6
-		}							// 7 8 9 10 
-		printf("\n");				// 11 12 13 14 15
+	for(i=1;i<=5;i++){
+		for(j=1;j<=i;j++){
+			printf("%d ",k);
+			k++;
+		}
+		printf("\n");
 	}
 	
 	printf("\n");
-	for(i=1;i<=5;i++){				//b)Out put
-		for(j=1;j<=i;j++){			// 1
-			if(a%2==0){				// 0 1
-				printf("0 ");		// 0 1 0
-			}						// 1 0 1 0
-			else{					// 1 0 1 0 1
+	for(i=1;i<=5;i++){
+		for(j=1;j<=i;j++){
+			if(a%2==0){
+				printf("0 ");
+			}
+			else{
 				printf("1 ");
 			}
 			a++;
@@ -28,23 +28,23 @@ void main(){
 	}
 		
 	printf("\n");
-	for(i=1;i<=5;i++){				//c)Out put
-		for(j=1;j<i;j++){			// * * * * *
-			printf(" ");			//  * * * *
-		}							//   * * *
-		for(j=i;j<=5;j++){			//    * *
-			printf("* ");			//     *
+	for(i=1;i<=5;i++){
+		for(j=1;j<i;j++){
+			printf(" ");
+		}
+		for(j=i;j<=5;j++){
+			printf("* ");
 		}
 	    printf("\n");
     }
     
     printf("\n");
-    for(i=1;i<=5;i++){				//d)Out put
-    	for(j=1;j<=5-i;j++){		//     1
-    		printf(" ");			//    A B
-		}							//   1 2 3
-		for(j=1;j<=i;j++){			//  C D E F
-			if(i%2==0){				// 1 2 3 4 5
+    for(i=1;i<=5;i++){
+    	for(j=1;j<=5-i;j++){
+    		printf(" ");
+		}
+		for(j=1;j<=i;j++){
+			if(i%2==0){
 				printf("%c ",A);
 				A++;
 			}
@@ -56,12 +56,12 @@ void main(){
 	}
 	
 	printf("\n");
-	for(i=1;i<=5;i++){							//e)Out put
-    	for(j=1;j<=5;j++){						// * * * * *
-			if(j==1 || j==5 || i==1 || i==5){	// *       *
-				printf("* ");					// *       *
-			}									// *       *
-			else{								// * * * * *
+	for(i=1;i<=5;i++){
+    	for(j=1;j<=5;j++){
+			if(j==1 || j==5 || i==1 || i==5){
+				printf("* ");	
+			}
+			else{
 				printf("  ");
 			}
 		}
