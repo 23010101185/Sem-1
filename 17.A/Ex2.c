abec8cbe88e0a4390948af8b51c1d092e705afbf
23010101185 <148247055+23010101185@users.noreply.github.com>
2023-10-27 08:36:34 +0530

    Add files via upload

--- 17.A/Ex2.c
+++ 17.A/Ex2.c
@@ -2,10 +2,10 @@
 
 #include<stdio.h>
 void main(){
-	int *a,n1;
-	float *b,n2;
-	double *c,n3;
-	char *d,n4;
+	int *a,*b,*c,*d,n1;
+	float n2;
+	double n3;
+	char n4;
 	printf("int n1 = ");
 	scanf("%d",&n1);
 	printf("float n2 = ");
@@ -13,13 +13,13 @@ void main(){
 	printf("double n3 = ");
 	scanf("%lf",&n3);
 	printf("char n4 = ");
-	scanf("%c",&n4);
+	scanf(" %c",&n4);
 	a=&n1;
 	b=&n2;
 	c=&n3;
 	d=&n4;
-	printf("int : %d %d",a,*a);
-	printf("\nfloat : %f %f",b,*b);
-	printf("\ndouble : %lf %lf",c,*c);
+	printf("\nint : %d %d",a,*a);
+	printf("\nfloat : %d %f",b,*b);
+	printf("\ndouble : %d %lf",c,*c);
 	printf("\nchar : %d %c",d,*d);
 }
